b8ca478b2a354be3813f769a5b65551a1b5cf545
ISE <ise.sns46@gmail.com>
2025-08-09 15:51:49 +0700

    Add initial UI

--- app/src/main/java/com/example/propertyvaluepredictor/MainActivity.kt
+++ app/src/main/java/com/example/propertyvaluepredictor/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.propertyvaluepredictor
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val houseTypes = arrayOf("Condo", "Townhouse", "Detached")
+        val adapter = ArrayAdapter(this, R.layout.list_item, houseTypes)
+        val houseTypeDropdown: AutoCompleteTextView = findViewById(R.id.house_type_auto_complete_text_view)
+
+        houseTypeDropdown.setAdapter(adapter)
     }
 }
